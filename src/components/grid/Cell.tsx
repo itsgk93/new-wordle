eeb07ae8dc846c74e0dcda3bb7166c3ded831efb
Gaurav Katiyar <gaukatiy1@WKMZT1B47888.global.publicisgroupe.net>
2022-02-26 22:16:46 +0530

    fixes

--- src/components/grid/Cell.tsx
+++ src/components/grid/Cell.tsx
@@ -24,9 +24,9 @@ export const Cell = ({
   const isHighContrast = getStoredIsHighContrastMode()
   const letterLength = localStorage.getItem('letterLength') ?? '5'
   const styleSize =
-    window.outerWidth <= 375 && letterLength === '6' ? '11' : '14'
+    window.outerWidth <= 375 && letterLength === '6' ? 'w-11 h-11' : 'w-14 h-14'
   const classes = classnames(
-    `w-${styleSize} h-${styleSize} border-solid border-2 flex items-center justify-center mx-0.5 text-4xl font-bold rounded dark:text-white`,
+    `${styleSize} border-solid border-2 flex items-center justify-center mx-0.5 text-4xl font-bold rounded dark:text-white`,
     {
       'bg-white dark:bg-slate-900 border-slate-200 dark:border-slate-600':
         !status,
